826b17a065c7d09083278300a6666f5592188f86
fherrerao <fherrera0206@gmail.com>
2022-10-02 20:28:55 -0500

    Added comment to explain the functionality of GetData class

--- get_data.rb
+++ get_data.rb
@@ -1,4 +1,5 @@
 class GetData
+  # Method to get the data from the file and separate the name from the schedule
   def self.data
     File.readlines('employees.txt').map { |line| line.split('=') }
   end
