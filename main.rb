516e899c761ba19b6e6081625ac2bb1badf10ebf
fherrerao <fherrera0206@gmail.com>
2022-10-14 08:41:13 -0500

    Used of repeated combination method to combine the employees

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require 'date'
 require './get_data'
-require './data_date'
+
 
 class Employee
   def initialize(data = GetData.data)
@@ -20,45 +20,30 @@ class Employee
   end
 
   def convert_to_hash
-    @data.to_h.each do |key, value|
-      @employees.push(key => value.split(','))
+    @data.each do |key, value|
       @match_employees.push(key)
     end
   end
 
-  def coincidence(next_item, one_employee_day)
-    two_employee_day = @employees[next_item][@match_employees[next_item]].filter do |element|
-      element[0..1] == one_employee_day[0..1]
-    end
-    return if two_employee_day.empty?
-
-    start_employee_one = DataDate.start_time(one_employee_day)
-    end_employee_one = DataDate.end_time(one_employee_day)
-    start_employee_two = DataDate.start_time(two_employee_day[0])
-    end_employee_two = DataDate.end_time(two_employee_day[0])
-
-    if start_employee_two.between?(start_employee_one, end_employee_one) ||
-       start_employee_one.between?(start_employee_two, end_employee_two)
-      @counter += 1
-    end
-  end
-
-  def display_coincidences
+  def display_coincidences    
     convert_to_hash
-
-    # Iterate object arrays to match the employees
-    (0..@match_employees.length - 1).each do |item|
-      (item + 1..@match_employees.length - 1).each do |next_item|
-        # Getting the schedules given by the employees (employees[i]['employee_name'])
-        @employees[item][@match_employees[item]].each do |one_employee_day|
-          coincidence(next_item, one_employee_day)
-        end
-        @result.push("#{@match_employees[item]}-#{@match_employees[next_item]}:#{@counter}")
-        @counter = 0
+    
+    @match_employees = @match_employees.repeated_combination(2).to_a
+    @match_employees.each do |employee|
+      next if employee[0] == employee[1]
+      @data[employee[0]].split(',').each do |item|
+        
+        employee_two = @data[employee[1]].split(',').filter {|item_2| item_2[0..1] == item[0..1]}  
+        next if employee_two.empty?
+        employee_two[0][2..6].between?(item[2..6], item[8..12]) || 
+        item[2..6].between?(employee_two[0][2..6], employee_two[0][8..12]) ? 
+        @counter += 1 : next
       end
+      p "#{employee[0]}-#{employee[1]} #{@counter}"
+      @counter = 0
     end
-    @result.each { |element| p element }
-  end
+    
+  end 
 end
 
 Employee.new.display_coincidences
