81bbb607d500f42838e86d6d1c57e89a8cc08eee
fherrerao <fherrera0206@gmail.com>
2022-10-14 08:40:17 -0500

    Gettign data and converting to hash

--- get_data.rb
+++ get_data.rb
@@ -1,6 +1,6 @@
 class GetData
   # Method to get the data from the file and separate the name from the schedule
   def self.data
-    File.readlines('employees.txt').map { |line| line.split('=') }
+    File.readlines('employees.txt').map { |line| line.split('=') }.to_h
   end
 end
