73b3f50fb9873bafbac998fbfced60449dbff583
fherrerao <fherrera0206@gmail.com>
2022-10-24 15:04:56 -0500

    Using repeated combination method instead of two loops

--- main.rb
+++ main.rb
@@ -1,7 +1,6 @@
 require 'date'
 require './get_data'
 
-
 class Employee
   def initialize(data = GetData.data)
     @data = data
@@ -20,30 +19,32 @@ class Employee
   end
 
   def convert_to_hash
-    @data.each do |key, value|
+    @data.each do |key, _value|
       @match_employees.push(key)
     end
   end
 
-  def display_coincidences    
+  def display_coincidences
     convert_to_hash
-    
     @match_employees = @match_employees.repeated_combination(2).to_a
     @match_employees.each do |employee|
       next if employee[0] == employee[1]
+
       @data[employee[0]].split(',').each do |item|
-        
-        employee_two = @data[employee[1]].split(',').filter {|item_2| item_2[0..1] == item[0..1]}  
+        employee_two = @data[employee[1]].split(',').filter { |item2| item2[0..1] == item[0..1] }
         next if employee_two.empty?
-        employee_two[0][2..6].between?(item[2..6], item[8..12]) || 
-        item[2..6].between?(employee_two[0][2..6], employee_two[0][8..12]) ? 
-        @counter += 1 : next
+
+        if employee_two[0][2..6].between?(item[2..6], item[8..12]) ||
+           item[2..6].between?(employee_two[0][2..6], employee_two[0][8..12])
+          @counter += 1
+        else
+          next
+        end
       end
       p "#{employee[0]}-#{employee[1]} #{@counter}"
       @counter = 0
     end
-    
-  end 
+  end
 end
 
 Employee.new.display_coincidences
